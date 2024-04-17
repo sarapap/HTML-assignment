951783eb046f97715e8bbe66e331e852dd47c3fd
sarapappila <sara.pappila@metropolia.fi>
2024-04-17 13:06:17 +0300

    Tallennetaan

--- javascript/kieli.js
+++ javascript/kieli.js
@@ -3,10 +3,10 @@
 document.getElementById("kieli").addEventListener("change", function () {
     var selectedLanguage = this.value;
     if (selectedLanguage === 'FI') {
-        window.location.href = '../fi/1Etusivu.html';
+        window.location.href = '../fi/page1.html';
     } else if (selectedLanguage === 'EN') {
-        window.location.href = '../en/1Etusivu_en.html';
+        window.location.href = '../en/page1_en.html';
     } else if (selectedLanguage === 'SV') {
-        window.location.href = "../sv/1Etusivu_sv.html";
+        window.location.href = "../sv/page1_sv.html";
     }
 });
