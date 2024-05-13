af47e6863471d7e2ead9a872a8930a7aa4f5d39c
sarapappila <sara.pappila@metropolia.fi>
2024-05-13 09:07:59 +0300

    korjataan linkki

--- html/javascript/kayttaja.js
+++ html/javascript/kayttaja.js
@@ -312,7 +312,7 @@ document.addEventListener('DOMContentLoaded', async function () {
 
     const storedProfilePic = localStorage.getItem('profilePictureFilename');
     if (storedProfilePic) {
-        profilePicture.src = `/uploads/${storedProfilePic}`;
+        profilePicture.src = `../../uploads/${storedProfilePic}`;
     } else {
         try {
             const response = await fetch(`http://localhost:3000/api/v1/users/avatar/${userID}`);
@@ -320,7 +320,7 @@ document.addEventListener('DOMContentLoaded', async function () {
                 const userData = await response.json();
 
                 const profilePictureFilename = userData.userPic;
-                profilePicture.src = `/uploads/${profilePictureFilename}`;
+                profilePicture.src = `../../uploads/${profilePictureFilename}`;
                 localStorage.setItem('profilePictureFilename', profilePictureFilename);
             }
         } catch (error) {
@@ -382,7 +382,7 @@ document.addEventListener('DOMContentLoaded', async function () {
             if (response.ok) {
                 const result = await response.json();
                 const profilePictureFilename = result.userPic;
-                profilePicture.src = `/uploads/${profilePictureFilename}`;
+                profilePicture.src = `../../uploads/${profilePictureFilename}`;
                 localStorage.setItem('profilePictureFilename', profilePictureFilename);
                 switch (selectedLanguage) {
                     case 'EN':
